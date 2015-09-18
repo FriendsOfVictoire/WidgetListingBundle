e2d296ac364ef2532efcab6ce6c35bca1e8ee682
Paul Andrieux <paul@appventus.com>
2015-09-18 11:57:32 +0200

    error when an address was lowercase

--- Resources/public/js/adressBook.js
+++ Resources/public/js/adressBook.js
@@ -30,7 +30,7 @@
         var orderedList = {'A': [], 'B': [], 'C': [], 'D': [], 'E': [], 'F': [], 'G': [], 'H': [], 'I': [], 'J': [], 'K': [], 'L': [], 'M': [], 'N': [], 'O': [], 'P': [], 'Q': [], 'R': [], 'S': [], 'T': [], 'U': [], 'V': [], 'W': [], 'X': [], 'Y': [], 'Z': []};
 
         for (var i = 0; i < listSort.length; i++) {
-            var initial = listSort[i]['name'].charAt(0);
+            var initial = listSort[i]['name'].charAt(0).toUpperCase();
             orderedList[initial].push(listSort[i]);
         }
         return orderedList;
