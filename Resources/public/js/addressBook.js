d47d89f66a05880b6c40ae77cec61cd75eb09786
Leny Bernard <leny@troopers.email>
2016-08-25 16:19:23 +0200

    fix typo adress => address

--- Resources/public/js/addressBook.js
+++ Resources/public/js/addressBook.js
@@ -1,20 +1,20 @@
 
 
-    function adressBookReplaceStringProto(proto, value, str) {
+    function addressBookReplaceStringProto(proto, value, str) {
         return proto.replace(new RegExp(str, 'g'), value);
     }
 
-    function adressBookDisplayList (list, widgetId) {
+    function addressBookDisplayList (list, widgetId) {
         var acronymProto = $('#' + widgetId + '-acronym-prototype').html();
         var pageProto = $('#' + widgetId + '-page-prototype').html();
         var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
         for (var i = 0; i < letters.length; i++) {
             if(list[letters[i]].length > 0 ){
-                var newAcronym = adressBookReplaceStringProto(acronymProto, letters[i] , "__acronym_initial__");
+                var newAcronym = addressBookReplaceStringProto(acronymProto, letters[i] , "__acronym_initial__");
                 $('#page-list-' + widgetId).append(newAcronym);
                 for (var j = 0; j < list[letters[i]].length; j++) {
-                    var newPage = adressBookReplaceStringProto(pageProto, list[[letters[i]]][j]['name'], "__page_name__");
-                    newPage = adressBookReplaceStringProto(newPage, list[[letters[i]]][j]['url'], "__page_url__");
+                    var newPage = addressBookReplaceStringProto(pageProto, list[[letters[i]]][j]['name'], "__page_name__");
+                    newPage = addressBookReplaceStringProto(newPage, list[[letters[i]]][j]['url'], "__page_url__");
                     $('#' + widgetId + letters[i]).append(newPage);
                 };
             }
@@ -22,7 +22,7 @@
         };
     }
 
-    function adressBookOrderList(list, sortProperty) {
+    function addressBookOrderList(list, sortProperty) {
         listSort = list.sort(function(a, b) {
             return a[sortProperty].localeCompare(b[sortProperty]);
         });
